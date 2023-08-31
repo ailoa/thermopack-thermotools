c04e568b35e0004617cd2a1182a10c0313a6a93f
Ailo Aasen <ailo.aasen@sintef.no>
2023-08-31 18:25:32 +0200

    Various features for (primarily) cubic EoS.
    a) Enable pseudocomponents
    b) Some more python wrappers
    c) Enable fifth-order polynomial volume shift.

--- addon/pyExamples/pseudocomponents.py
+++ addon/pyExamples/pseudocomponents.py
@@ -18,10 +18,12 @@ kijmat = [[eos.get_kij(i,j) for i in cindices] for j in cindices]
 pressure1 = eos.pressure_tv(temp=150, volume=1e-2, n=[1.0,2.0], dpdt=True, dpdv=True)
 print (pressure1)
 
-# Recreate the same mixture using pseudo functionality. Two initializations required:
-# The first one tells thermopack which components are pseudocomponents.
-# The second initializes the pseudocomponents using Tc, Pc, acf, Mw. The real components are left untouched.
-# NB: Ideal gas heat capacities are not implemented for pseudocomponents, so only residual caloric properties are available.
+# Recreate the same mixture using pseudo functionality. Two initializations
+# required: The first one tells thermopack which components are
+# pseudocomponents. The second initializes the pseudocomponents using Tc, Pc,
+# acf, Mw. The real components are left untouched. NB: Ideal gas heat capacities
+# are not implemented for pseudocomponents, so only residual caloric properties
+# are available.
 eos = cubic("C1,PSEUDO", "PR", mixing="HV") 
 eos.init_pseudo(comps="C1,ArbitraryName", Tclist=Tclist, Pclist=Pclist, acflist=acflist, Mwlist=Mwlist)
 _ = [[eos.set_kij(i,j,kijmat[i-1][j-1]) for i in cindices] for j in cindices]
